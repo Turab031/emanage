f68e491628c7e8311f3903dda3db0aab0fd32b36
Turab031 <147339712+Turab031@users.noreply.github.com>
2026-03-06 00:10:41 +0530

    dept

--- src/app/pages/department/department.ts
+++ src/app/pages/department/department.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { DepartmentModel } from '../../models/Department.model';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Master } from '../../services/master';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-department',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './department.html',
   styleUrl: './department.css',
 })
-export class Department {
-  newDeptObj:DepartmentModel= new DepartmentModel();
-
-  onSaveDept(){
-    
+export class Department implements OnInit {
+  newDeptObj: DepartmentModel = new DepartmentModel();
+  masterService = inject(Master);
+  deptList = signal<DepartmentModel[]>([])
+  ngOnInit(): void {
+    this.getAllDepartments();
   }
 
+  onSaveDept() {
+    this.masterService.saveDept(this.newDeptObj).subscribe(({
+      next:(result:any)=>{
+        alert("department created success")
+        this.deptList.set(result)
 
+      },
+      error:(error)=>{
+
+
+      }
+
+    }))
+  }
+  getAllDepartments() {
+    this.masterService.getAllDept().subscribe({
+      next: (result: any) => {
+        this.deptList.set(result);
+      },
+    });
+  }
 }
--- src/app/services/master.ts
+++ src/app/services/master.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable ,inject} from '@angular/core';
+import { DepartmentModel } from '../models/Department.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Master {
+  apiUrl:string="https://localhost:7074/api"
+  http= inject(HttpClient)
+
+  getAllDept(){
+    return this.http.get(this.apiUrl+"/DepartmentMaster/GetAllDepartments")
+  }
+
+  saveDept(obj:DepartmentModel){
+    return this.http.post(this.apiUrl+"/DepartmentMaster/AddDepartment",obj)
+  }
   
 }
